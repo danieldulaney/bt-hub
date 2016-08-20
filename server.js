76c87c10cdd65e93cbfef6487b09d25307d6c6b7
danieldulaney <dulaney.daniel@gmail.com>
2016-08-20 11:22:36 -0400

    Fixed bug where page could only be requested once.

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ var http = require('http');
 var fs = require('fs');
 var os = require('os');
 
-var readFile = fs.createReadStream("./web/index.html");
-
 var staticServer = http.createServer(function(req, res){
 	console.log('Got ' + req.method + ' request for ' + req.url);
 
 	res.status = 200;
 
-	readFile.pipe(res);
+	fs.createReadStream("./web/index.html").pipe(res);
 });
 
 staticServer.listen(80, function(){
